ec0bf270a220867fd2c3a0c26ddf6f8c965cbfe7
wsowy <51901804+alobre@users.noreply.github.com>
2025-05-25 17:06:49 +0200

    feedService working feed Page working TODO: Design

--- headlinr-ui/src/app/pages/feed/feed.component.ts
+++ headlinr-ui/src/app/pages/feed/feed.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { RssFeedOutputDto } from '../../models/rss-feed.model';
+import { Item } from '../../models/rss-feed.model';
+import { FeedService } from '../../services/feed.service';
 
 @Component({
   selector: 'app-feed',
@@ -16,48 +17,15 @@ import { RssFeedOutputDto } from '../../models/rss-feed.model';
     NzButtonModule,
     NzIconModule
   ],
-  templateUrl: './feed.component.html'
+  templateUrl: './feed.component.html',
 })
-export class FeedComponent {
-  feedItems: RssFeedOutputDto = {
-  version: '2.0',
-  title: 'Headlinr News Feed',
-  link: 'https://headlinr.com',
-  description: 'Latest tech headlines',
-  language: 'en-us',
-  items: [
-    {
-      guid: { isPermaLink: true, value: 'https://headlinr.com/article-1' },
-      title: 'Breaking: Angular 19 Released',
-      description: 'Explore all the latest Angular 19 features now available.',
-      pubDate: '2025-05-25',
-      link: 'https://angular.io',
-      group: {
-        contents: [
-          { width: 640, url: 'https://angular.io/assets/images/logos/angular/angular.svg' }
-        ],
-        credit: 'Angular Team'
-      },
-      mrssContents: [],
-      inlineContent: { width: 640, url: '' },
-      mrssCredit: 'Angular',
-      inlineCredit: 'Angular'
-    },
-    {
-      guid: { isPermaLink: true, value: 'https://headlinr.com/article-2' },
-      title: 'NG-ZORRO Best Practices',
-      description: 'UI design tips using NG-ZORRO in Angular apps.',
-      pubDate: '2025-05-24',
-      link: 'https://ng.ant.design',
-      group: {
-        contents: [],
-        credit: ''
-      },
-      mrssContents: [],
-      inlineContent: { width: 640, url: 'https://ng.ant.design/assets/img/logo.svg' },
-      mrssCredit: 'NG-ZORRO Team',
-      inlineCredit: 'Design Team'
-    }
-  ]
-};
+export class FeedComponent implements OnInit {
+  feedService = inject(FeedService);
+  feedItems: Item[] = []
+
+  async ngOnInit() {
+    this.feedService.getFeed().subscribe(feed => {
+      this.feedItems = feed;
+    });
+  }
 }
--- headlinr-ui/src/app/services/feed.service.ts
+++ headlinr-ui/src/app/services/feed.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RssFeedOutputDto } from '../models/rss-feed.model';
+import { Item } from '../models/rss-feed.model';
 
 @Injectable({ providedIn: 'root' })
 export class FeedService {
   constructor(private http: HttpClient) {}
 
-  getFeed(): Observable<RssFeedOutputDto> {
-    return this.http.get<RssFeedOutputDto>('https://your-backend.com/api/rss');
+  getFeed(): Observable<Item[]> {
+    const response = this.http.get<Item[]>('https://localhost:7152/api/News/GetAllNews');
+    
+    return response;
   }
 }
